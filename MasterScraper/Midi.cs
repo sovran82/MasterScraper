86a6ceaed1f53a586fdb1470ef60eb4cdc5ce081
Ujjal Das <sovran82@gmail.com>
2017-08-13 04:35:37 +0600

    geetabitan.com midi files scraper done

---
+++ MasterScraper/Midi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterScraper
+{
+    class Midi
+    {
+        public string Link;
+        public string FileName;
+        public string Name;
+        public string Category;     //ex.English/Solo/Bon_Jovi
+        public byte[] FileByte;
+
+    }
+}
