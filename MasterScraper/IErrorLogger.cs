f4fc191c1d3c70521b9646b3cd02251e51ba87e8
Ujjal Das <sovran82@gmail.com>
2017-08-28 22:18:40 +0600

    paglaworld.me midi files scraper done

---
+++ MasterScraper/IErrorLogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics; 
+
+namespace MasterScraper
+{
+    interface IErrorLogger
+    {
+        void Log(string error);
+    }
+
+    class OutputWindowLogger : IErrorLogger
+    {
+        public void Log(string error)
+        {
+            Debug.WriteLine(error);
+        }
+    }
+
+    class ConsoleLogger : IErrorLogger
+    {
+        public void Log(string error)
+        {
+            Console.WriteLine(error);
+        }
+    }
+}
